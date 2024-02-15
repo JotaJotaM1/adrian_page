dd9ffa340c9a93cc02c2fc9c0dd7b427ab20ee48
JotaJotaM1 <johnjames_89@hotmail.com>
2024-02-14 20:33:14 -0500

    The indentation of the entire project is adjusted

--- script.js
+++ script.js
@@ -13,55 +13,55 @@ close.addEventListener("click", () => {
 
 /* Script to validate and submit form data as JSON, displaying success or error alerts with SweetAlert2. */
 
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('contact-form');
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contact-form");
 
-  form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent the default form submission
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
 
-      form.classList.add('was-validated'); // Activate Bootstrap visual validation
+    form.classList.add("was-validated"); // Activate Bootstrap visual validation
 
-      if (form.checkValidity()) {
-          // Collect form data and convert it to a JSON object
-          const formData = new FormData(form);
-          const data = Object.fromEntries(formData.entries());
+    if (form.checkValidity()) {
+      // Collect form data and convert it to a JSON object
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData.entries());
 
-          fetch('http://localhost:3000/send-email', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json', // Specify that the body is JSON
-              },
-              body: JSON.stringify(data) // Convert form data to a JSON string
-          })
-          .then(response => response.json())
-          .then(data => {
-              if(data.success) {
-                  Swal.fire({
-                      title: 'Success!',
-                      text: data.message,
-                      icon: 'success',
-                      confirmButtonText: 'Ok'
-                  });
-              } else {
-                  Swal.fire({
-                      title: 'Error',
-                      text: 'The email could not be sent.',
-                      icon: 'error',
-                      confirmButtonText: 'Ok'
-                  });
-              }
-          })
-          .catch((error) => {
-              console.error('Error:', error);
-              Swal.fire({
-                  title: 'Error',
-                  text: 'There was a problem sending the email.',
-                  icon: 'error',
-                  confirmButtonText: 'Ok'
-              });
+      fetch("http://localhost:3000/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json", // Specify that the body is JSON
+        },
+        body: JSON.stringify(data), // Convert form data to a JSON string
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            Swal.fire({
+              title: "Success!",
+              text: data.message,
+              icon: "success",
+              confirmButtonText: "Ok",
+            });
+          } else {
+            Swal.fire({
+              title: "Error",
+              text: "The email could not be sent.",
+              icon: "error",
+              confirmButtonText: "Ok",
+            });
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          Swal.fire({
+            title: "Error",
+            text: "There was a problem sending the email.",
+            icon: "error",
+            confirmButtonText: "Ok",
           });
-      } else {
-          console.error('The form is invalid.');
-      }
+        });
+    } else {
+      console.error("The form is invalid.");
+    }
   });
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const express = require('express');
-const sgMail = require('@sendgrid/mail');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const sgMail = require("@sendgrid/mail");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
@@ -12,10 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post('/send-email', async (req, res) => {
-    console.log(req.body);
-    const { name, lastname, email, phonenumber, message, address } = req.body;
-    const content = `
+app.post("/send-email", async (req, res) => {
+  console.log(req.body);
+  const { name, lastname, email, phonenumber, message, address } = req.body;
+  const content = `
         Name: ${name} ${lastname}
         Email: ${email}
         Phone: ${phonenumber}
@@ -23,23 +23,24 @@ app.post('/send-email', async (req, res) => {
         Address: ${address}
     `;
 
-    const msg = {
-        to: process.env.EMAIL_TO,
-        from: process.env.EMAIL_FROM,
-        subject: 'New contact message',
-        text: content,
-    };
+  const msg = {
+    to: process.env.EMAIL_TO,
+    from: process.env.EMAIL_FROM,
+    subject: "New contact message",
+    text: content,
+  };
 
-    try {
-        await sgMail.send(msg);
-        res.json({ success: true, message: "Email sent successfully" });
-    } catch (error) {
-        console.error('Error sending the email:', error);
-        res.status(500).json({ success: false, message: "Error sending the email" });
-    }
+  try {
+    await sgMail.send(msg);
+    res.json({ success: true, message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending the email:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error sending the email" });
+  }
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
